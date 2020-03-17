fff619b887a7935381edf12bb1ce0351c401d9be
Paul Morgan III <pmorgan3@gmail.com>
2020-03-17 13:07:12 -0500

    added margin slider for container

--- src/components/user/Container.tsx
+++ src/components/user/Container.tsx
@@ -3,13 +3,13 @@ import { Paper, FormControl, FormLabel, Slider } from '@material-ui/core';
 import { useNode } from '@craftjs/core';
 import ColorPicker from 'material-ui-color-picker';
 
-export const Container: any = ({ background, padding = 0, children }) => {
+export const Container: any = ({ background, padding = 0, children, width, margin }) => {
   const { connectors: { connect, drag } } = useNode();
   const wrapper = (r: any) => connect(drag(r))
   return (
       <Paper
           ref={(ref) => wrapper(ref)}
-          style={{ background, padding: `${padding}px` }
+          style={{ background, padding: `${padding}px`, width: `${width}%`, margin: `${margin}%` }
           }
       >
       {children}
@@ -17,9 +17,11 @@ export const Container: any = ({ background, padding = 0, children }) => {
   );
 };
 export const ContainerSettings = () => {
-  const { background, padding, setProp } = useNode(node => ({
+  const { background, padding, setProp, width, margin } = useNode((node) => ({
     background: node.data.props.background,
-    padding: node.data.props.padding
+    padding: node.data.props.padding,
+    width: node.data.props.width,
+    margin: node.data.props.margin
   }));
   return (
     <div>
@@ -32,6 +34,10 @@ export const ContainerSettings = () => {
       <FormControl fullWidth={true} margin="normal" component="fieldset">
         <FormLabel component="legend">Padding</FormLabel>
         <Slider defaultValue={padding} onChange={(_, value) => setProp(props => props.padding = value)} />
+        <FormLabel component="legend">Width</FormLabel>
+        <Slider defaultValue={width} onChange={(_, value) => setProp(props => props.width = value)} />
+        <FormLabel component="legend">Margin</FormLabel>
+        <Slider defaultValue={margin} onChange={(_, value) => setProp(props => props.margin = value)} />
       </FormControl>
     </div>
   )
