73c112f7b460ce5054834fae14e77c07f1f7a932
Paul Morgan III <pmorgan3@gmail.com>
2020-05-24 21:06:05 -0500

    Updates

---
+++ precache-manifest.bc594a31d0cc61fe9653da0908b4c3fd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8b500f7c52dd1d39be22602292b386f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5042706571029ac9713",
+    "url": "/static/css/main.9c235c5a.chunk.css"
+  },
+  {
+    "revision": "319e874ed0313e8c4e31",
+    "url": "/static/js/2.dc0a4c45.chunk.js"
+  },
+  {
+    "revision": "b5042706571029ac9713",
+    "url": "/static/js/main.3a22212d.chunk.js"
+  },
+  {
+    "revision": "daaf555a5f4f374c4938",
+    "url": "/static/js/runtime-main.fe9361a4.js"
+  }
+]);
