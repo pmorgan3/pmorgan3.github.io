7a550cbfe58ee5b4ac8e698e88d6e3d57b092069
Paul Morgan III <pmorgan3@gmail.com>
2020-10-12 12:50:15 -0500

    fixed meta tag

---
+++ build/precache-manifest.6e75753418075c7ca8b9dbab1d36debb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53592e6dda2ff7c3b463fecc79d47139",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f486af7d24d265d99faf",
+    "url": "/static/css/main.63f0c094.chunk.css"
+  },
+  {
+    "revision": "d2531453b0748cc052f5",
+    "url": "/static/js/2.0456fece.chunk.js"
+  },
+  {
+    "revision": "f486af7d24d265d99faf",
+    "url": "/static/js/main.12c53170.chunk.js"
+  },
+  {
+    "revision": "5f20caf09d1c2705c39c",
+    "url": "/static/js/runtime-main.e2a69350.js"
+  }
+]);
