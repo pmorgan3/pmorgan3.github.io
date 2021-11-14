0a089aad8c2c22bc00a4248a8806ccef41f3042d
Paul Morgan III <pmorgan3@gmail.com>
2021-11-14 13:07:57 -0600

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bc9097aa49017097f2e738d5f8389401.js"
+  "/precache-manifest.fa44183a3241effaf983913129d2a45c.js"
 );
 
 self.addEventListener('message', (event) => {
