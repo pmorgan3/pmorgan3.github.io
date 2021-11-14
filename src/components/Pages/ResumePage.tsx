343600c21a1ee4b0a33e0179b91b071edaca1606
Paul Morgan III <pmorgan3@gmail.com>
2021-11-14 12:41:32 -0600

    Updated resume

--- src/components/Pages/ResumePage.tsx
+++ src/components/Pages/ResumePage.tsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import * as projects from '../../text'
-const ResumeHeader: React.FC<{ header: string }> = ({ header }) => {
-  return <div className='ResumeHeader'>{header}</div>
-}
-const ResumeSubHeader: React.FC<{ subHeader: string }> = ({ subHeader }) => {
-  return <div className='ResumeSubHeader'>{subHeader}</div>
-}
-const ResumeDate: React.FC<{ dateRange: string }> = ({ dateRange }) => {
-  return <div className='ResumeDate'>{dateRange}</div>
-}
-const ResumeDescription: React.FC<{ description: string }> = ({
+import React from 'react';
+import * as projects from '../../text';
+const ResumeHeader: React.FC<{ header: string; }> = ({ header }) => {
+  return <div className='ResumeHeader'>{ header }</div>;
+};
+const ResumeSubHeader: React.FC<{ subHeader: string; }> = ({ subHeader }) => {
+  return <div className='ResumeSubHeader'>{ subHeader }</div>;
+};
+const ResumeDate: React.FC<{ dateRange: string; }> = ({ dateRange }) => {
+  return <div className='ResumeDate'>{ dateRange }</div>;
+};
+const ResumeDescription: React.FC<{ description: string; }> = ({
   description,
 }) => {
-  return <div className='ResumeDescription'>{description}</div>
-}
-const SkillList: React.FC<{ skills: string[] }> = ({ skills }) => {
+  return <div className='ResumeDescription'>{ description }</div>;
+};
+const SkillList: React.FC<{ skills: string[]; }> = ({ skills }) => {
   return (
     <>
-      {skills.map((s) => (
-        <div className='Skills'> - {s}</div>
-      ))}
+      { skills.map((s) => (
+        <div className='Skills'> - { s }</div>
+      )) }
     </>
-  )
-}
+  );
+};
 const Experience: React.FC = () => {
   const skills = [
     'C# and the .Net platform',
@@ -31,13 +31,17 @@ const Experience: React.FC = () => {
     'Reactjs and Redux.js',
     'C/C++',
     'Docker',
-  ]
+  ];
   return (
     <>
       <ResumeHeader header='Experience' />
+      <ResumeSubHeader subHeader='Reveal Data: Front-End Engineer' />
+      <ResumeDate dateRange='August 2021 - present' />
+      <ResumeDescription description='As a Front-End Engineer at Reveal I work with a team to help build applications that better help our customers visualize and make sense of large sets of data.' />
+      <br />
       <ResumeSubHeader subHeader='Webitects: Software Engineer' />
-      <ResumeDate dateRange='May 2021 - Present' />
-      <ResumeDescription description='As a Software engineer I worked directly with clients to create full stack web applications that meet their needs.'/>
+      <ResumeDate dateRange='May 2021 - August 2021' />
+      <ResumeDescription description='As a Software engineer I worked directly with clients to create full stack web applications that meet their needs.' />
       <br />
 
       <ResumeSubHeader subHeader='Webitects: Software Developer Intern' />
@@ -48,28 +52,28 @@ const Experience: React.FC = () => {
       />
       <br />
       <ResumeHeader header='Projects' />
-      <ResumeSubHeader subHeader={projects.keyvTitle} />
-      <ResumeDate dateRange={'Available via npm'} />
-      <ResumeDescription description={projects.keyvBody} />
+      <ResumeSubHeader subHeader={ projects.keyvTitle } />
+      <ResumeDate dateRange={ 'Available via npm' } />
+      <ResumeDescription description={ projects.keyvBody } />
 
-      <ResumeSubHeader subHeader={projects.legalDocketTitle} />
+      <ResumeSubHeader subHeader={ projects.legalDocketTitle } />
       <ResumeDate dateRange='Offline. Code available upon request' />
-      <ResumeDescription description={projects.legalDocketBody} />
+      <ResumeDescription description={ projects.legalDocketBody } />
 
-      <ResumeSubHeader subHeader={projects.uicSchoolPageTitle} />
+      <ResumeSubHeader subHeader={ projects.uicSchoolPageTitle } />
       <ResumeDate dateRange='Offline. Code available upon request' />
-      <ResumeDescription description={projects.uicSchoolPageBody} />
+      <ResumeDescription description={ projects.uicSchoolPageBody } />
 
-      <ResumeSubHeader subHeader={projects.androidNoteTitle} />
+      <ResumeSubHeader subHeader={ projects.androidNoteTitle } />
       <ResumeDate dateRange='Code available on github' />
-      <ResumeDescription description={projects.androidNoteBody} />
+      <ResumeDescription description={ projects.androidNoteBody } />
       <br />
       <ResumeHeader header='Skills and Abilities' />
-      <ResumeSubHeader subHeader={'Proficient in:'} />
-      <SkillList skills={skills} />
+      <ResumeSubHeader subHeader={ 'Proficient in:' } />
+      <SkillList skills={ skills } />
     </>
-  )
-}
+  );
+};
 export const ResumePage: React.FC = () => {
   return (
     <>
@@ -78,5 +82,5 @@ export const ResumePage: React.FC = () => {
         <Experience />
       </div>
     </>
-  )
-}
+  );
+};
