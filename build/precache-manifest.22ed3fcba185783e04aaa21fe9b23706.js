0cad7e0e9d6b5acddfbe0d5e6eb88df1d46f502c
Paul Morgan III <pmorgan3@gmail.com>
2022-10-11 21:55:26 -0500

    push build

---
+++ build/precache-manifest.22ed3fcba185783e04aaa21fe9b23706.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a7bc2e0e8cc5f54e35bbf982784d62",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c4718ce341e3d2292332",
+    "url": "/static/css/main.6c67d8a5.chunk.css"
+  },
+  {
+    "revision": "7c9a3cb08f203fb435f4",
+    "url": "/static/js/2.7dea40a2.chunk.js"
+  },
+  {
+    "revision": "c4718ce341e3d2292332",
+    "url": "/static/js/main.5e581dec.chunk.js"
+  },
+  {
+    "revision": "5f20caf09d1c2705c39c",
+    "url": "/static/js/runtime-main.e2a69350.js"
+  }
+]);
